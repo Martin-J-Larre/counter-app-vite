ebacf65a4802ddf55fa2f86ce4bfdc4f11b0fbe1
Martin-J-Larre <martinjlarre@gmail.com>
2022-10-21 13:00:05 -0400

    #1 Init

---
+++ README.md
@@ -0,0 +1,12 @@
+# Counter App
+
+### Built with
+
+- React
+- Vite
+- Prop-types
+
+## Demo
+
+- Website - [Add your name here](https://www.your-site.com)
+
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import CounterApp from './components/CounterApp'
+
+function App() {
+
+  return (
+    <div className="App">
+      <CounterApp value={ 10 } />
+    </div>
+  )
+}
+
+export default App
